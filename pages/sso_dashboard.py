48425407ef6b737f266b8f8ed7bba4cd4cdc8bfb
viorelaioia <viorelaioia@gmail.com>
2018-06-28 16:54:32 +0300

    Wait a bit

--- pages/sso_dashboard.py
+++ pages/sso_dashboard.py
@@ -32,8 +32,10 @@ class SsoDashboard(Base):
         return TwoFactorAuthenticationPage(self.base_url, self.selenium)
 
     def click_discourse(self, message):
+        time.sleep(2)
         self.selenium.find_element(*self._discourse_app_locator).click()
         print (self.selenium.window_handles)
+        time.sleep(2)
         self.selenium.switch_to.window(self.selenium.window_handles[1])
         auth = Auth0(self.base_url, self.selenium)
         auth.wait_for_message(message)
