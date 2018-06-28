5459d85faadbd1323deeca04715fa3fbfdffd572
viorelaioia <viorelaioia@gmail.com>
2018-06-28 09:33:55 +0300

    try a sleep

--- pages/sso_dashboard.py
+++ pages/sso_dashboard.py
@@ -32,6 +32,7 @@ class SsoDashboard(Base):
         return TwoFactorAuthenticationPage(self.base_url, self.selenium)
 
     def click_discourse(self, message):
+        time.sleep(3)
         self.selenium.find_element(*self._discourse_app_locator).click()
         self.selenium.switch_to.window(self.selenium.window_handles[1])
         auth = Auth0(self.base_url, self.selenium)
